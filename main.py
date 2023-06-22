99590ee324bd07df2f271e055380af945c851d4e
JosephAxe <95534308+JosephAxe@users.noreply.github.com>
2023-06-22 14:28:39 -0300

    Criando mais tipagens

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 # flake8 main.py
 # mypy main.py
 
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
-fila_teste = FilaNormal()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-print(fila_teste.chama_cliente(5))
-print(fila_teste.chama_cliente(5))
 
-fila_teste_2 = FilaPrioritaria()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-print(fila_teste_2.chama_cliente(10))
-print(fila_teste_2.chama_cliente(1))
-print(fila_teste_2.estatistica('01/01/2001', 215, 'detail'))
+# fila_teste = FilaNormal()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# print(fila_teste.chama_cliente(5))
+# print(fila_teste.chama_cliente(5))
+#
+# fila_teste_2 = FilaPrioritaria()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# print(fila_teste_2.chama_cliente(10))
+# print(fila_teste_2.chama_cliente(1))
+# print(fila_teste_2.estatistica('01/01/2001', 215, 'detail'))
+
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
