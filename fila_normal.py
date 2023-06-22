6c3aab5156f6c4ae31c2eae194434a9124749295
JosephAxe <95534308+JosephAxe@users.noreply.github.com>
2023-06-22 11:34:30 -0300

    Criando metodo template

--- fila_normal.py
+++ fila_normal.py
@@ -2,6 +2,7 @@ from fila_base import FilaBase
 
 
 class FilaNormal(FilaBase):
+
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'NM{self.codigo}'
 
--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -2,13 +2,9 @@ from fila_base import FilaBase
 
 
 class FilaPrioritaria(FilaBase):
+
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'P{self.codigo}'
-
-    def atualiza_fila(self) -> None:
-        self.reseta_fila()
-        self.gera_senha_atual()
-        self.fila.append(self.senha_atual)
 
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
